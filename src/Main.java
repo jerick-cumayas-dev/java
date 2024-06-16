4d57630ccfe1ef85bbd0a3d12d8d013dfe924bca
Jerick <cumayas.jerick@gmail.com>
2024-06-16 19:33:23 +0800

    allow update users

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,83 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         ArrayList<Employee> users = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        int input;
         populateUsers(users);
-        displayUsers(users);
 
+        do {
+            displayUsers(users);
+            showInstructions();
+            input = scanner.nextInt();
+            showOptions();
+            int option = scanner.nextInt();
+            updateUser(users, input, option);
+        } while (input != -1);
+
+    }
+
+    public static void showInstructions(){
+        System.out.print("Input index of user to edit: ");
+    }
+
+    public static void showOptions(){
+        System.out.println("Menu: ");
+        System.out.println("1. First Name");
+        System.out.println("2. Middle Name");
+        System.out.println("3. Last Name");
+        System.out.println("4. Salary");
+        System.out.println("5. Department");
+        System.out.print("Choice: ");
+    }
+
+    public static void updateUser(ArrayList<Employee> users, int input, int option){
+        Scanner scanner = new Scanner(System.in);
+        Employee updatedUser = null;
+
+        for (int i = 0; i < users.size(); i++) {
+            if (i == input){
+                updatedUser = users.get(i);
+            }
+        }
+
+        switch (option){
+            case 1:
+                try {
+                    System.out.print("Input new first name: ");
+                    if (updatedUser != null){
+                        updatedUser.setFirstName(scanner.nextLine());
+                    }
+
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+                break;
+            case 2:
+                    System.out.print("Input new middle name: ");
+                    assert updatedUser != null;
+                    updatedUser.setMiddleName(scanner.nextLine());
+                break;
+            case 3:
+                    System.out.print("Input a new last name: ");
+                    assert updatedUser != null;
+                    updatedUser.setLastName(scanner.nextLine());
+                break;
+            case 4:
+                    System.out.print("Input a new salary: ");
+                    assert updatedUser != null;
+                    updatedUser.setSalary(scanner.nextFloat());
+                break;
+            case 5:
+                    System.out.print("Input a new department: ");
+                    assert updatedUser != null;
+                    updatedUser.setDepartment(scanner.nextLine());
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + option);
+        }
     }
 
     public static void populateUsers(ArrayList<Employee> users){
